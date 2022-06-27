f93904353312b381bda3c6c4356fb536564c7e3c
MSF ROR <edactic@gmail.com>
2022-06-27 20:36:08 +0000

    Added Partials and Destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,7 +42,13 @@ class ArticlesController < ApplicationController
   def show
    @article = Article.find(params[:id])
   end
-    
+ 
+  def destroy 
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article Successfully deleted"
+    redirect_to articles_path 
+  end
     
   private
   
